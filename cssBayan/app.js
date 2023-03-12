5ef5a699ac642350c9ae1ed147d55413f0cc54ec
aleksandramosyagina <a.mosyagina@list.ru>
2023-03-12 17:46:35 +0300

    feat: modified files (Sun, Mar 12, 2023, 17:46:20)

--- cssBayan/app.js
+++ cssBayan/app.js
@@ -1,12 +1,8 @@
 const btn = document.getElementById("first_block");
-const x_element = document.getElementById('x_element');
-const plus_element = document.getElementById('plus_element');
 
-btn.onclick = function() {
+btn.onclick = function () {
     let element = document.getElementById("first_image");
     element.classList.toggle("main__image_hidden");
-    x_element.classList.toggle("main__image_x");
-    plus_element.classList.toggle("main__image_plus_hidden");
 }
 
 let btn2 = document.getElementsByClassName('main__block');
@@ -14,9 +10,9 @@ console.dir(btn2);
 let images2 = document.querySelectorAll('.main__image_hidden');
 console.dir(images2);
 for (let i = 0; i < btn2.length; i++) {
-    let elem = btn2[i+1];
+    let elem = btn2[i + 1];
     console.log(elem);
-    elem.addEventListener("click", function() {
+    elem.addEventListener("click", function () {
         images2[i].classList.toggle('main__image_hidden');
     })
 }
